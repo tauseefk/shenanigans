3b7e801dd4ef3bff1d2a9b8b22d64b72dc860f9e
Tauseef Khan <tauseefk@gmail.com>
2017-04-28 22:56:34 -0400

    shenanigans with task deletion

---
+++ app/components/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Notes from './Notes';
+import uuid from 'uuid';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const {notes} = this.state;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} onDelete={this.deleteNote}/>
+      </div>
+    );
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New Task'
+      }])
+    });
+  }
+
+  deleteNote = (id, e) => {
+    e.stopPropagation();
+    this.setState({
+      notes: this.state.notes.filter(note => note.id != id)
+    });
+  }
+}
